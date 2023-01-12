d98cb04966cf6927ac566180808bf209ba14dcc4
Alumno <emmanuel.maciel0208@gmail.com>
2023-01-12 10:28:06 -0600

    Medabot Emmanuel

--- Assets/Scripts/SpawnManagerScriptableObject.cs
+++ Assets/Scripts/SpawnManagerScriptableObject.cs
@@ -6,11 +6,9 @@ public class SpawnManagerScriptableObject : ScriptableObject
     public string prefabName;
 
     public int numberOfPrefabsToCreate;
-<<<<<<< HEAD
-    public Vector4[] spawnPoints;
-=======
+
     public Vector3[] spawnPoints;
     public int soyHacker = 10000; //esto lo hizo el crack de Abel xD e.e
     
->>>>>>> 40094a78dc0c857f8e598a192160058871f1067f
+
 }
